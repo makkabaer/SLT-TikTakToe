084ba384d44b285338bb9bea4514b8f528fe956f
Markus <wi24b012@technikum-wien.atm>
2025-05-31 15:51:39 +0200

    Upload Test4

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.printf("Emil Simeonov ");
+        System.out.printf("Emil Simeonov2 ");
         //TEST
     }
 }
