7ce3ebe7abb0c5b1502f29fb3fe21708a39b6c30
Peter Kovacs <wi24b183@technikum-wien.at>
2025-06-09 21:19:07 +0200

    Rückgrat des Programs

--- test.java
+++
@@ -1,7 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.printf("Emil Simeonov ");
-        System.out.printf("Emil Simeonov2 ");
-        //TEST
-    }
-}
